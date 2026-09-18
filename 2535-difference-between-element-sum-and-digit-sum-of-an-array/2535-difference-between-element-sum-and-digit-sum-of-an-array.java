8863e39acbbdc76995d584ba5c6bda2ad1b10524
Serenity Buckner <116240430+constance07@users.noreply.github.com>
2026-09-18 10:34:59 -0400

    Time: 2 ms (98.64%), Space: 46.8 MB (11.58%) - LeetHub

--- 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.java
+++ 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.java
@@ -3,13 +3,13 @@ class Solution {
         int elementSum = 0;
         int digitSum = 0;
 
-        for(int num: nums){
+        for (int num : nums) {
             elementSum += num;
         }
 
-        for(int i = 0; i < nums.length; i++){
-            while(true){
-                if(nums[i] < 10){
+        for (int i = 0; i < nums.length; i++) {
+            while (true) {
+                if (nums[i] < 10) {
                     digitSum += nums[i];
                     break;
                 }
@@ -18,7 +18,6 @@ class Solution {
                 nums[i] = nums[i] / 10;
             }
         }
-
         return Math.abs(elementSum - digitSum);
     }
 }
