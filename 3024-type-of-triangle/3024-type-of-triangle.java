9e0c0ff3e3ab40e7f6c8e876ec6650900ecdebac
Serenity Buckner <116240430+constance07@users.noreply.github.com>
2026-09-20 10:14:35 -0400

    Time: 0 ms (100%), Space: 43.7 MB (72.71%) - LeetHub

---
+++ 3024-type-of-triangle/3024-type-of-triangle.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String triangleType(int[] nums) {
+        String triangle = "";
+        boolean isTriangle = false;
+
+        if((nums[0] + nums[2]) > nums[1] && (nums[0] + nums[1]) > nums[2] && (nums[1] + nums[2]) > nums[0]){
+            isTriangle = true;
+            
+            if(nums[0] == nums[1]){
+                if(nums[0] == nums[2]){
+                    triangle = "equilateral";
+                }else{
+                    triangle = "isosceles";
+                }
+            }else if(nums[1] == nums[2]){
+                if(nums[0] == nums[2]){
+                    triangle = "equilateral";
+                }else{
+                    triangle = "isosceles";
+                }
+            }else if(nums[0] == nums[2]){
+                if(nums[0] == nums[1]){
+                    triangle = "equilateral";
+                }else{
+                    triangle = "isosceles";
+                }
+            }else{
+                triangle = "scalene";
+            }  
+        }
+
+        if(isTriangle){
+            return triangle;
+        }
+        return "none";
+    }
+}
