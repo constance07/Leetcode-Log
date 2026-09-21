4e4f3b55a6af11cab5ecd961f67c5e5ff8ae9f63
Serenity Buckner <116240430+constance07@users.noreply.github.com>
2026-09-21 10:09:40 -0400

    Time: 10 ms (33.47%), Space: 47.8 MB (20.3%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] < 0){
+                nums[i] *= -1;
+            }
+            nums[i] = nums[i] * nums[i];
+        }
+        
+        Arrays.sort(nums);
+        return nums;
+    }
+}
