91cb5ad3b9bee063c1b4a2ad8644170c57ab810f
Serenity Buckner <116240430+constance07@users.noreply.github.com>
2026-09-24 21:05:04 -0400

    Time: 2 ms (95.33%), Space: 43.5 MB (66.43%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -4,7 +4,6 @@ class Solution {
         int sum = 0;
 
         for (int i = 0; i < operations.length; i++) {
-
             if(operations[i].equals("+") && record.size() >= 2){
                 record.add((record.get(record.size() - 1)) + (record.get(record.size() - 2)));
             }else if(operations[i].equals("D") && record.size() >= 1){
@@ -14,7 +13,7 @@ class Solution {
             }else if(operations.length > 0){
                 record.add(Integer.parseInt(operations[i]));
             }
-        }//5, 10
+        }
         
         for (int i : record) {
             sum += i;
